a6f642578dc0362236253793090ec388afe17ec1
OktayCopurlu <selimhalim12@gmail.com>
2022-01-25 22:08:29 +0100

    birseyler yaptik

--- about.js
+++ about.js
@@ -1,3 +1,8 @@
 function doSomething() {
   console.log("selam arkadaslar");
 }
+function doSomething2() {
+    console.log('baska birsey yazdi');
+}
+
+console.log('burada conflict olmamasi gerekiyor.');
